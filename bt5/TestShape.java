d75882f7ff4c241e797495e01ebfb79fde698fbc
Thuận NM <150407855+kenduNMT@users.noreply.github.com>
2025-03-25 21:21:44 +0700

    Update TestShape.java

--- bt5/TestShape.java
+++ bt5/TestShape.java
@@ -2,11 +2,9 @@ package bt5;
 
 class TestShape {
     public static void main(String[] args) {
-        // Tạo đối tượng Circle
         Circle myCircle = new Circle(5);
         System.out.println("Diện tích hình tròn: " + myCircle.getArea());
 
-        // Tạo đối tượng Cylinder
         Cylinder myCylinder = new Cylinder(5, 10);
         System.out.println("Thể tích hình trụ: " + myCylinder.getVolume());
 
